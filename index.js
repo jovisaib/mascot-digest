d617884e3e83f45f8539ba25736b690057e7d9bc
jvsaeznayar <jv.saez@nayarsystems.com>
2019-04-09 11:44:53 +0200

    updated to now 2

--- index.js
+++ index.js
@@ -1,52 +1,42 @@
 const http = require('http');
+const Twitter = require("twitter");
 const config = require("./config.js");
 const databaseUri = require("./database.js");
-const Twitter = require("twitter");
 const { MongoClient } = require("mongodb");
 
 const mdbClient = new MongoClient(databaseUri, { useNewUrlParser: true });
 const twitterClient = new Twitter(config);
-const params = { screen_name: 'mondomascots', exclude_replies: true, count: 1 };
-let last = "";
-let db = null;
-
-let connectToMongo = () => {
-    return mdbClient.connect();
-}
+const params = { screen_name: 'mondomascots', exclude_replies: true, count: 1, include_rts: false };
 
 
-let getMascotStatus = () => {
+http.createServer((req, res) => {
+    let last = "";
+
     twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
             tweets.forEach(t => {
                 last = t.entities.media[0].media_url_https;
-                db.collection("mascots_raw").insertOne(t, function(err, res) {
-                    if (err) throw err;
-                    console.log("MASCOT: ", last);
-                });
+
+                mdbClient.connect()
+                    .then(() => {
+                        mdbClient.db("mascots").collection("mascots_raw").insertOne(t, function (err, res) {
+                            if (err) throw err;
+                        });
+                        console.log("MASCOT ", last);
+                    })
+                    .catch(err => console.log(err));
             })
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            var html = buildHtml(last);
+            res.end(html);
         }
     });
-}
-
-http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    var html = buildHtml(req);
-    res.end(html);
-}).listen(8080);
 
 
-connectToMongo()
-    .then(()=>{
-        db = mdbClient.db("mascots");
-        getMascotStatus();
-        setInterval(getMascotStatus, 1000 * 60 * 15);
-    })
-    .catch(err => console.log(err));
+}).listen(8081);
 
 
-function buildHtml(req) {
+function buildHtml(last) {
     return '<!DOCTYPE html>'
         + '<html><head>'
         + `<img src="${last}"></img>`
