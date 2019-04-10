a1d6ffa8919e5dc8e68ed6093a51ded58b47e77b
jvsaeznayar <jv.saez@nayarsystems.com>
2019-04-10 16:46:02 +0200

    db insertion improved

--- index.js
+++ index.js
@@ -6,35 +6,45 @@ const { MongoClient } = require("mongodb");
 
 const mdbClient = new MongoClient(databaseUri, { useNewUrlParser: true });
 const twitterClient = new Twitter(config);
-const params = { screen_name: 'mondomascots', exclude_replies: true, include_rts: false, count: 1 };
+const params = { screen_name: 'mondomascots', exclude_replies: true, include_rts: false, tweet_mode: 'extended', count: 1 };
 
 
 http.createServer((req, res) => {
     let last = "";
 
     twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
-        let tweet = null;
         if (!error) {
-            tweets.forEach(t => {
-                if (t.entities.media && t.entities.media.length) {
-                    tweet = t;
-                    last = t.entities.media[0].media_url_https;
-                }
-            })
+            let db = null;
+            mdbClient.connect()
+                .then(() => {
+                    db = mdbClient.db("mascots");
 
-            if (tweet) {
-                mdbClient.connect()
-                    .then(() => {
-                        mdbClient.db("mascots").collection("mascots_raw").insertOne(tweet, function (err, res) {
-                            if (err) throw err;
-                        });
-                        console.log("MASCOT ", last);
+
+                    tweets.forEach(t => {
+                        let tweet = null;
+                        if (t.entities.media && t.entities.media.length) {
+                            tweet = t;
+                            last = t.entities.media[0].media_url_https;
+                            console.log(last)
+                        }
+
+                        if (tweet) {
+                            db.collection("mascots_raw").find({ id_str: t.id_str }, {}).count(function (err, amount) {
+                                if (err) throw err;
+                                if (amount >= 1) return
+
+                                db.collection("mascots_raw").insertOne(tweet, function (err, res) {
+                                    if (err) throw err;
+                                });
+                            });
+                        }
                     })
-                    .catch(err => console.log(err));
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            var html = buildHtml(last);
-            res.end(html);
+
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    var html = buildHtml(last);
+                    res.end(html);
+                })
+                .catch(err => console.log(err));
         }
     });
 }).listen();
