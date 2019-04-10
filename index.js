061531624083eb4b222b81e4fe016041ede3be31
jvsaeznayar <jv.saez@nayarsystems.com>
2019-04-10 10:38:12 +0200

    wip

--- index.js
+++ index.js
@@ -6,34 +6,38 @@ const { MongoClient } = require("mongodb");
 
 const mdbClient = new MongoClient(databaseUri, { useNewUrlParser: true });
 const twitterClient = new Twitter(config);
-const params = { screen_name: 'mondomascots', exclude_replies: true, count: 1, include_rts: false };
+const params = { screen_name: 'mondomascots', exclude_replies: true, include_rts: false, count: 1 };
 
 
 http.createServer((req, res) => {
     let last = "";
 
     twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
+        let tweet = null;
         if (!error) {
             tweets.forEach(t => {
-                last = t.entities.media[0].media_url_https;
+                if (t.entities.media && t.entities.media.length) {
+                    tweet = t;
+                    last = t.entities.media[0].media_url_https;
+                }
+            })
 
+            if (tweet) {
                 mdbClient.connect()
                     .then(() => {
-                        mdbClient.db("mascots").collection("mascots_raw").insertOne(t, function (err, res) {
+                        mdbClient.db("mascots").collection("mascots_raw").insertOne(tweet, function (err, res) {
                             if (err) throw err;
                         });
                         console.log("MASCOT ", last);
                     })
                     .catch(err => console.log(err));
-            })
+            }
             res.writeHead(200, { 'Content-Type': 'text/html' });
             var html = buildHtml(last);
             res.end(html);
         }
     });
-
-
-}).listen(8081);
+}).listen();
 
 
 function buildHtml(last) {
